06727e9dcc2aaf94f785491bf9cf53a4c5829575
ibtissam09 <ibtissam.makdoun@gmail.com>
2023-01-11 17:19:12 +0100

    Create extract_education_level function to extract level of education and update database

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 from database_management.database import *
 import re
 from datetime import datetime, timedelta
-
+from statistics import mean
 
 def fix_date():
     # Select data from database
@@ -28,4 +28,54 @@ def fix_date():
             print(df.loc[i, "tags"][0])
     # Convert date_publication to date type
     df['date_publication'] = pd.to_datetime(df['date_publication'])
+    return df
+
+def extract_education_level():
+    df = select_data_with_filter('niveau_etude', 'null')
+    for i in range(0, len(df)):
+        description = df.loc[i, 'description']['desc']
+        if re.search('bachelor', description.lower()):
+            df.loc[i, "niveau_etude"] = 3
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("bac\+\d+", description.lower()):
+            if len(re.findall("bac\+\d+", description.lower())) == 1:
+                niveau_etude = re.findall("bac\+\d+", description.lower())[0]
+                niveau_etude = int(re.findall('\d+', niveau_etude)[0])
+                df.loc[i, "niveau_etude"] = niveau_etude
+                update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+            else:
+                niveau_etude = []
+                for n in re.findall("bac\+\d+", description.lower()):
+                    niveau_etude.append(int(re.findall('\d+', n)[0]))
+                df.loc[i, "niveau_etude"] = int(mean(niveau_etude))
+                update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("bac\s*\+\s*\d+", description.lower()):
+            if len(re.findall("bac\s*\+\s*\d+", description.lower())) == 1:
+                niveau_etude = re.findall("bac\s*\+\s*\d+", description.lower())[0]
+                niveau_etude = int(re.findall('\d+', niveau_etude)[0])
+                df.loc[i, "niveau_etude"] = niveau_etude
+                update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+            else:
+                niveau_etude = []
+                for n in re.findall("bac\s*\+\s*\d+", description.lower()):
+                    niveau_etude.append(int(re.findall('\d+', n)[0]))
+                df.loc[i, "niveau_etude"] = int(mean(niveau_etude))
+                update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("bac plus \d+", description.lower()):
+            print(re.findall("bac plus \d+", description.lower()))
+        elif re.search("ingénieur", description.lower()):
+            df.loc[i, "niveau_etude"] = 5
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("ingenieur", description.lower()):
+            df.loc[i, "niveau_etude"] = 5
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("engineer", description.lower()):
+            df.loc[i, "niveau_etude"] = 5
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("\bmasters\b", description.lower()):
+            df.loc[i, "niveau_etude"] = 5
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
+        elif re.search("technicien", description.lower()):
+            df.loc[i, "niveau_etude"] = 3
+            update_value('_id', df.loc[i, '_id'], 'niveau_etude', df.loc[i, "niveau_etude"])
     return df
